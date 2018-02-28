411cdc47ffd9a1bad86e70e128190f4f2b0588b5
Stanislau Alimpau <alimberg@list.ru>
2018-02-28 19:13:21 +0300

    advanced-zeros/feature: adding code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let maxDiv = base;
+  for (let i=maxDiv; i>1; i--) {
+    if (maxDiv%i==0) {
+      maxDiv = i;
+    }
+  }
+  let countMaxDiv = 0;
+  let countMaxBase = base;
+  while (true) {
+    if (countMaxBase % maxDiv == 0) {
+      countMaxDiv++;
+      countMaxBase = countMaxBase / maxDiv;
+    } else {
+      break
+    }
+  }
+  let power = 0;
+  let powerMax = 1;
+  while (true) {
+    powerMax *= maxDiv;
+    power += Math.floor(number/powerMax);
+    if (number/powerMax <= 1)
+      break
+  }
+    return Math.floor(power/countMaxDiv)
 }
